78ef709e490a70284556124e31587e1e78947048
GhifariRizkiRamadhan <bmada2011@gmail.com>
2025-01-14 08:30:42 +0700

    erorr keren

--- controller/conRiwayat.js
+++ controller/conRiwayat.js
@@ -1,6 +1,6 @@
 const showRiwayat = async (req, res) => {
     try {
-      res.render("riwayat");
+      res.render("riayat");
     } catch (error) {
       console.log("error", error);
       res.status(500).json({ message: error });
