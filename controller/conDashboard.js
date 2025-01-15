a39496fb96415d0d8ae40f497cde286c215383c1
mhdharsya <m.harsya2004@gmail.com>
2025-01-15 08:22:39 +0700

    update lagi

--- controller/conDashboard.js
+++ controller/conDashboard.js
@@ -1,19 +1,4 @@
 const showDashboard = async (req, res) => {
-<<<<<<< HEAD
-  try {
-     res.render("dashboard", { activePage: "dashboard" });
-  } catch (error) {
-    console.log("error", error);
-    res.status(500).json({ message: error });
-  }
-};
-
-
-
-module.exports = {
-  showDashboard
-};
-=======
     try {
        res.render("dashboard", { activePage: "dashboard" });
     } catch (error) {
@@ -27,4 +12,3 @@ module.exports = {
   module.exports = {
     showDashboard
   };
->>>>>>> f644da7e24a7d7286d7b8aa05083a3aa48409d51
