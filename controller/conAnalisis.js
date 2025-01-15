ef5b5694b22f5d635fb650f7dbd93221db9533f9
mhdharsya <m.harsya2004@gmail.com>
2025-01-15 08:18:27 +0700

    reset ulang

--- controller/conAnalisis.js
+++ controller/conAnalisis.js
@@ -1,14 +1,14 @@
 const showAnalisis = async (req, res) => {
-    try {
-      res.render("analisis.ejs");
-    } catch (error) {
-      console.log("error", error);
-      res.status(500).json({ message: error });
-    }
-  };
-  
-  
-  
-  module.exports = {
-    showAnalisis
-  };
+  try {
+    res.render("analisis", { activePage: "analisis"});
+  } catch (error) {
+    console.log("error", error);
+    res.status(500).json({ message: error });
+  }
+};
+
+
+
+module.exports = {
+  showAnalisis
+};
--- controller/conDashboard.js
+++ controller/conDashboard.js
@@ -1,14 +1,14 @@
 const showDashboard = async (req, res) => {
-    try {
-      res.render("dashboard");
-    } catch (error) {
-      console.log("error", error);
-      res.status(500).json({ message: error });
-    }
-  };
-  
-  
-  
-  module.exports = {
-    showDashboard
-  };
+  try {
+     res.render("dashboard", { activePage: "dashboard" });
+  } catch (error) {
+    console.log("error", error);
+    res.status(500).json({ message: error });
+  }
+};
+
+
+
+module.exports = {
+  showDashboard
+};
