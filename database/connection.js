e91a810a5cc9969bcc6fb1f75aca04bdee16c31a
mhdharsya <m.harsya2004@gmail.com>
2025-01-16 13:36:10 +0700

    database

--- database/connection.js
+++ database/connection.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const sql = require('mssql');
 
 const config = {
-    user:process.env.AZURE_SQL_USER,
+    user: process.env.AZURE_SQL_USER,
     password: process.env.AZURE_SQL_PASSWORD,
     server: process.env.AZURE_SQL_SERVER,
     database: process.env.AZURE_SQL_DATABASE,
