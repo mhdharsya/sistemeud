0cf3aeeb81d538bf14e35f4ef571de983bc61cfc
mhdharsya <m.harsya2004@gmail.com>
2025-02-02 19:47:42 +0700

    edit

--- controller/conDashboard.js
+++ controller/conDashboard.js
@@ -1,6 +1,5 @@
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
-const { getDashboardData } = require("../controller/conChart");
 
 const showDashboard = async (req, res) => {
   try {
