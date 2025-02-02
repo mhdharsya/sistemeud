e4deb093a1c1a66435add02abef41f81432bd78e
mhdharsya <m.harsya2004@gmail.com>
2025-02-02 20:13:55 +0700

    update

--- controller/conRiwayat.js
+++ controller/conRiwayat.js
@@ -3,17 +3,42 @@ const prisma = new PrismaClient();
 
 const showRiwayat = async (req, res) => {
   try {
-    // Ambil data MalwareFile dan relasinya, seperti nama file, jenis malware, status analisis, dll.
+    // Ambil page dan limit dari query parameter, jika tidak ada gunakan default value
+    const page = parseInt(req.query.page) || 1; // Halaman saat ini
+    const limit = 10; // Menampilkan 10 data per halaman
+
+    // Hitung offset untuk pagination
+    const offset = (page - 1) * limit;
+
+    // Ambil data MalwareFile yang sudah dianalisis dan relasinya dengan limit dan offset
     const malwareFiles = await prisma.malwareFile.findMany({
+      where: {
+        status: "Analyzed", // Menambahkan kondisi status "Analyzed"
+      },
+      skip: offset,
+      take: limit,
       include: {
         analysis: true, // Mengambil data relasi analysisResult
       },
     });
 
+    // Hitung total data malware dengan status "Analyzed"
+    const totalFiles = await prisma.malwareFile.count({
+      where: {
+        status: "Analyzed", // Hanya menghitung data yang statusnya "Analyzed"
+      },
+    });
+    
+    // Hitung total halaman
+    const totalPages = Math.ceil(totalFiles / limit);
+
     // Kirim data ke view 'riwayat'
     res.render("riwayat", {
       activePage: "riwayat",
       malwareFiles,
+      currentPage: page, // Kirim halaman saat ini
+      totalPages, // Kirim total halaman
+      totalFiles, // Kirim total data
     });
   } catch (error) {
     console.log("error", error);
